00869ad27304e2b5ac5f203a4534357125f76d46
Joel Tennant <joel_tennant@hotmail.com>
2013-06-02 21:16:29 +1200

    Pulling picture from file system to display on page. Can upload a picture from website

--- FarmPhoto/FarmPhoto.Website/Controllers/HomeController.cs
+++ FarmPhoto/FarmPhoto.Website/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
 using FarmPhoto.Core;
 using FarmPhoto.Domain;
+using FarmPhoto.Website.Models;
 
 namespace FarmPhoto.Website.Controllers
 {
@@ -19,11 +23,29 @@ namespace FarmPhoto.Website.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-            _photoManager.CreatePhoto(new Photo {Title = "Title"});
+            return View(new PictureModel()); 
+        }
 
-            return View();
+        
+
+        public ActionResult GetMostRecentImage()
+        {
+
+            ImageConverter converter = new ImageConverter();
+
+            Image myImg = Image.FromFile(@"D:\Users\Joel\Documents\GitHub\FarmPhoto\FarmPhoto\FarmPhoto.Website\App_Data\Uploads\117699.jpg");
+
+            FilePathResult holder =
+                File(
+                    @"D:\Users\Joel\Documents\GitHub\FarmPhoto\FarmPhoto\FarmPhoto.Website\App_Data\Uploads\117699.jpg",
+                    "image/jpg");
+            return holder; 
+
+           // return (byte[])converter.ConvertTo(myImg, typeof(byte[]));
+
+            //byte[] image = (byte[]) converter.ConvertTo(myImg, typeof (byte[])); 
+            //return File(image, "image/jpg");
         }
 
         public ActionResult About()
